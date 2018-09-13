73a67212dec57d65f46ef55682459c9b11d1c954
Jack Cook <hello@jackcook.nyc>
2018-09-13 15:07:41 -0400

    Initial commit

---
+++ schedule.js
@@ -0,0 +1,21 @@
+let options = document.getElementsByClassName("option");
+
+for (var i = 0; i < options.length; i++) {
+  options[i].onclick = function(e) {
+    let option = e.target.parentElement.getAttribute("option");
+    let rows = document.getElementsByTagName("tr");
+
+    for (var j = 0; j < rows.length; j++) {
+      let rowOption = rows[j].getAttribute("option");
+
+      if (rowOption === option) {
+        rows[j].classList.remove("hidden");
+      } else if (rowOption !== null) {
+        rows[j].classList.add("hidden");
+      }
+    }
+
+    options[parseInt(option, 10)].classList.add("selected");
+    options[1 - parseInt(option, 10)].classList.remove("selected");
+  };
+}
